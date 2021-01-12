aa7b0c94d90b3e84eb8e088b98713d38bab7eeeb
Laszlo-Andras Zsurzsa <zsurzalaszlo@gmail.com>
2021-01-13 00:34:53 +0100

    Enhance deployment of local development environment. Add documentation.

--- ui-application/client/src/App.js
+++ ui-application/client/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {apiResponse: ""};
+    this.state = {apiResponse: "Waiting for response ..."};
   }
 
   callAPI() {
